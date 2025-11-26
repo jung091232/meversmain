4f3dd950e5bab4f44d58f910467a79db5a5d831d
jung091232 <jcy091232@gmail.com>
2025-11-26 11:15:50 +0900

    Add files via upload

--- mevers-next-app/pages/_app.tsx
+++ mevers-next-app/pages/_app.tsx
@@ -1,12 +1,18 @@
 import type { AppProps } from "next/app";
 import { WagmiConfig } from "wagmi";
 import { config } from "../src/wagmi";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { useState } from "react";
 import "../styles/style.css";
 
 export default function MyApp({ Component, pageProps }: AppProps) {
+  const [queryClient] = useState(() => new QueryClient());
+
   return (
     <WagmiConfig config={config}>
-      <Component {...pageProps} />
+      <QueryClientProvider client={queryClient}>
+        <Component {...pageProps} />
+      </QueryClientProvider>
     </WagmiConfig>
   );
 }
